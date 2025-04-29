9a2c003774c389b3aeed028758c6802e3cb28226
pmsu2007 <pmsu2007@naver.com>
2025-04-29 16:03:54 +0900

    fix: DB 연결 확인 로직 추가

--- v3/utils/db.js
+++ v3/utils/db.js
@@ -1,5 +1,6 @@
 require("dotenv").config(); // .env 파일 로드
 const { Pool } = require("pg");
+const { logger } = require("./logger");
 
 // PostgreSQL Connection Pool;
 let pool;
@@ -11,7 +12,19 @@ if (!pool) {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    max: process.env.DB_MAX_CONNECTION
   });
+
+  pool.connect()
+      .then(client => {
+        logger.info("[PostgreSQL] DB 연결 성공");
+        client.release();
+      })
+      .catch(err => {
+        logger.error("[PostgreSQL] DB 연결 실패:", err.stack);
+        process.exit(1);
+      });
+
 }
 
 module.exports = pool;
